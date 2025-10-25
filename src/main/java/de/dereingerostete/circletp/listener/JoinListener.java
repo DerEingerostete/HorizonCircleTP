d4bef730900698ea5bad625b876c87ee46552a75
DerEingerostete <git@dereingerostete.dev>
2025-10-25 13:31:36 +0200

    refactor: Use PlayerSpawnLocationEvent instead of PlayerJoinEvent

--- src/main/java/de/dereingerostete/circletp/listener/JoinListener.java
+++ src/main/java/de/dereingerostete/circletp/listener/JoinListener.java
@@ -6,15 +6,15 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
+import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 @RequiredArgsConstructor
 public class JoinListener implements Listener {
     private final @NotNull RespawnHelper helper;
 
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerJoin(@NotNull PlayerJoinEvent event) {
+    public void onPlayerJoin(@NotNull PlayerSpawnLocationEvent event) {
         Player player = event.getPlayer();
         if (player.hasPlayedBefore()) return; // Only new players
 
@@ -23,7 +23,7 @@ public class JoinListener implements Listener {
                 helper.getRandomRespawn();      // Random respawn location
 
         if (location != null) {
-            player.teleport(location);
+            event.setSpawnLocation(location);
         }
     }
 
